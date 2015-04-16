24fae0093fa5bc4b6507567e70271d1e36d11ac9
haydenelza <hayden.elza@gmail.com>
2015-04-16 14:24:21 -0500

    Added maptools lib to handle as() coercion functions, fixed variable naming convention to _ instead of CamelCase, fixed general formatting, added as.ppp() for each dataset as is now working properly due to the inclusion of maptools

--- rileys_k.R
+++ rileys_k.R
@@ -2,32 +2,35 @@
 install.packages("spatstat")
 library("spatstat")
 install.packages("rgdal")
-library("rgdal") # Need this to import shapefiles
+library("rgdal")  # Need this to import shapefiles
+install.packages("maptools")
+library("maptools")  # Need this because it contains the as() coercion functions
 
 # Set working directory
-# setwd("/home/ygritte/Classes/GEOG_578/project_data/")
+#setwd("/home/ygritte/Classes/GEOG_578/project_data/")
 setwd("c:/Users/student/Downloads/project_data/")
-# setwd("c:/path/to/steves/inferior/windows/working/directory/")
+#setwd("c:/path/to/steves/working/directory/")
+
+# Read in point shape files for research intensity and university location data
+research_intensity <- readOGR(".", "research_frequency_points") # Point data will be easier
+uni_points <- readOGR(getwd(), "CollegesUniversities")
 
 
-### Read in raster or *point* shape file with research intensity data.
-researchIntensity <- readOGR(".","research_frequency_points") # Point data will be easier
-#print(proj4string(UniPoints)) # Report the projection information as string
-#plot(researchIntensity, pch=20, cex=0.8)
-
-### Read in raster or point shape file with university locations.
+# Tests to view projection, metadata and plots
+#print(proj4string(research_intensity)) # Report the projection information as string
+#print(proj4string(uni_points))
 #ogrInfo(".", "CollegesUniversities") # Report info about dataset
-UniPoints <- readOGR(".","CollegesUniversities")
-#print(proj4string(UniPoints)) # Report the projection information as string
-#plot(UniPoints, pch=20, cex=0.8)
+#plot(research_intensity, pch=20, cex=0.8)
+#plot(uni_points, pch=20, cex=0.8)
+
 
 # Transform data into dataframe with fields latitude, longitude, and # of resarch instances
 
 # Separate dataframes into subsets. Each value of # of research instances.
 
 # Transform each dataframe into an R ppp point pattern object.
-uni_points <- as.ppp(UniPoints)
-uni_points <- as(UniPoints, "ppp")
+uni_points_ppp <- as.ppp(uni_points)
+research_intensity_ppp <- as.ppp(research_intensity)
 
 # (Might need to combine university point and resarch instance data)
 
